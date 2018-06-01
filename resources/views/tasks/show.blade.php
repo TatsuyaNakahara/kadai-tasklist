6a52f0997824b5e4c55dac82bf0799d756d2d702
TatsuyaNakahara <tatsuya.nakahara@rakuten.com>
2018-06-01 04:47:21 +0000

    bootstrap design

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -8,9 +8,17 @@
     <p>{{ $task->content }}</p>
 
     {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id]) !!}
-
-    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
-    {!! Form::close() !!}
-
+  <div class="row">
+      <div class="col-xs-12">
+      <div class="col-sm-8 col-sm-offset-2">
+      <div class="col-md-8 col-md-offset-2">
+      <div class="col-lg-6 col-lg-offset-3">
+             {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+               {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+             {!! Form::close() !!}
+      </div>
+      </div>
+      </div>
+      </div>
+  </div>
 @endsection
