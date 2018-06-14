5e7d0faa57304360d15e6117b86eaa9765dd9ca6
TatsuyaNakahara <tatsuya.nakahara@rakuten.com>
2018-06-14 07:00:37 +0000

    migrate

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -63,7 +63,6 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
-            'status' => 'required|max:191',
             'content' => 'required|max:191',
         ]);
         $task = Task::find($id);
