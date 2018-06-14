ffaaa15cbcb0d1ca24660d63c386503f5a801c50
TatsuyaNakahara <tatsuya.nakahara@rakuten.com>
2018-06-14 07:31:55 +0000

    save point

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id: {{ $task->id }} のタスク編集ページ</h1>
+  <div class="container">
+    <div class="row">
+        <div class="col-xs-12"></div>
+        <div class="col-sm-8 col-sm-offset-2"></div>
+        <div class="col-md-8 col-md-offset-2"></div>
+        <div class="col-lg-6 col-lg-offset-3"></div>
+    </div>
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+        
+            {!! Form::close() !!}
+  </div>
+
+@endsection
